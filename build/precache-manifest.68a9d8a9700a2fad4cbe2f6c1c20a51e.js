0d8203acbd73bc1aaa9ebfd116d7fbf78157c0a7
student <romanilichev@mail.ru>
2019-04-14 23:44:54 +0300

    пуш билда

---
+++ build/precache-manifest.68a9d8a9700a2fad4cbe2f6c1c20a51e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "9b95e6c6858a34ae5015",
+    "url": "/static/css/main.4102730c.chunk.css"
+  },
+  {
+    "revision": "9b95e6c6858a34ae5015",
+    "url": "/static/js/main.11b5f9e7.chunk.js"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.c5541365.js"
+  },
+  {
+    "revision": "ec1d6227db8db2e08a3f",
+    "url": "/static/js/2.58bb1f55.chunk.js"
+  },
+  {
+    "revision": "f6451297fa905f63e95de34470e2f302",
+    "url": "/index.html"
+  }
+];
