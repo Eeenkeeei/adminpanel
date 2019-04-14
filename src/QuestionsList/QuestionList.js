c7251f5fdecf6627ecacc9c5d2c5f57b2a0894cf
student <romanilichev@mail.ru>
2019-04-14 11:32:58 +0300

    получение списка вопросов в state QuestionList

--- src/QuestionsList/QuestionList.js
+++ src/QuestionsList/QuestionList.js
@@ -1,11 +1,13 @@
 import React, {Component} from 'react';
 import QuestionElement from "../QuestionElement/QuestionElement";
 import User from "../model/User";
+import Question from "../model/Question";
 
-export default class QuestionList extends Component{
+export default class QuestionList extends Component {
 
     state = {
         users: [],
+        questions: []
     }; // <- ES10: 2019 (Babel)
 
     componentDidMount() {
@@ -20,21 +22,44 @@ export default class QuestionList extends Component{
             .then(res => res.json())
             .then(
                 (result) => {
+                    let objectToPush;
                     for (const resultElement of result) {
+                        if (resultElement.support.length !== 0) {
+
+                            resultElement.support.map((object) => {
+
+                                objectToPush = new Question(resultElement.username, object.theme, object.question, object.status )
+
+                                this.setState({
+
+                                    questions: [...this.state.questions, objectToPush]
+                                });
+
+                            })
+                        }
+
                         this.setState({
-                            users: [...this.state.users, new User(resultElement)]
+                            users: [...this.state.users, new User(resultElement)],
+
                         });
                     }
+                    console.log(this.state.users)
+
                 },
                 (error) => {
                     console.log(error)
                 }
             )
+
     }
-    render(){
-        return(
+
+
+    render() {
+        console.log(this.state.questions);
+
+        return (
             <div>
-                {this.state.users.map(value => <QuestionElement key={value.username} item={value}/>)}
+                {/*{this.state.users.map(value => <QuestionElement key={value.username} item={value}/>)}*/}
             </div>
         )
     }
