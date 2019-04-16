b83d819d3377c9801cf85d6a8fbff752d617bacd
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-04-16 13:16:16 +0000

    Deploy Eeenkeeei/adminpanel to github.com/Eeenkeeei/adminpanel.git:gh-pages

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.13f515aed0c773b8bd47a24ee3bdbd01.js"
+  "/precache-manifest.a3ce27d2ad73553b3b9040af96970227.js"
 );
 
 workbox.clientsClaim();
