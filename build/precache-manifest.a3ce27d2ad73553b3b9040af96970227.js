cb30d3cb6af860ec6fbb11c4224f8af31b29ce6a
student <romanilichev@mail.ru>
2019-04-16 16:51:25 +0300

    фикс gh pages

---
+++ build/precache-manifest.a3ce27d2ad73553b3b9040af96970227.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "af1f02d83cf5d7ec96d9",
+    "url": "/static/css/main.07590172.chunk.css"
+  },
+  {
+    "revision": "af1f02d83cf5d7ec96d9",
+    "url": "/static/js/main.deb3317d.chunk.js"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.c5541365.js"
+  },
+  {
+    "revision": "2aeb3e70744ce9e591f9",
+    "url": "/static/css/2.f5930875.chunk.css"
+  },
+  {
+    "revision": "2aeb3e70744ce9e591f9",
+    "url": "/static/js/2.09f1eae8.chunk.js"
+  },
+  {
+    "revision": "8da466a37bf2d6b1201e14d52f9510e8",
+    "url": "/index.html"
+  }
+];
